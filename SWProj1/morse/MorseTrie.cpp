73ab2c7da26a7ebdd51f1a384a6eea5d46294835
mmtrinidad4 <159863680+mmtrinidad4@users.noreply.github.com>
2024-04-18 09:07:51 +0800

    Update MorseTrie.cpp

--- SWProj1/morse/MorseTrie.cpp
+++ SWProj1/morse/MorseTrie.cpp
@@ -6,50 +6,22 @@ MorseTrie::MorseTrie() {
     // MAPPINGS
     
     // CAPS
-    char_to_morse['A'] = ".-";
-    char_to_morse['B'] = "-...";
-    char_to_morse['C'] = "-.-.";
-    char_to_morse['D'] = "-..";
-    char_to_morse['E'] = ".";
-    char_to_morse['F'] = "..-.";
-    char_to_morse['G'] = "--.";
-    char_to_morse['H'] = "....";
-    char_to_morse['I'] = "..";
-    char_to_morse['J'] = ".---";
-    char_to_morse['K'] = "-.-";
-    char_to_morse['L'] = ".-..";
-    char_to_morse['M'] = "--";
-    char_to_morse['N'] = "-.";
-    char_to_morse['O'] = "---";
-    char_to_morse['P'] = ".--.";
-    char_to_morse['Q'] = "--.-";
-    char_to_morse['R'] = ".-.";
-    char_to_morse['S'] = "...";
-    char_to_morse['T'] = "-";
-    char_to_morse['U'] = "..-";
-    char_to_morse['V'] = "...-";
-    char_to_morse['W'] = ".--";
-    char_to_morse['X'] = "-..-";
-    char_to_morse['Y'] = "-.--";
-    char_to_morse['Z'] = "--..";
+    char_to_morse['A'] = ".-"; char_to_morse['B'] = "-..."; char_to_morse['C'] = "-.-."; char_to_morse['D'] = "-..";
+    char_to_morse['E'] = "."; char_to_morse['F'] = "..-."; char_to_morse['G'] = "--."; char_to_morse['H'] = "....";
+    char_to_morse['I'] = ".."; char_to_morse['J'] = ".---"; char_to_morse['K'] = "-.-"; char_to_morse['L'] = ".-..";
+    char_to_morse['M'] = "--"; char_to_morse['N'] = "-."; char_to_morse['O'] = "---"; char_to_morse['P'] = ".--.";
+    char_to_morse['Q'] = "--.-"; char_to_morse['R'] = ".-."; char_to_morse['S'] = "..."; char_to_morse['T'] = "-";
+    char_to_morse['U'] = "..-"; char_to_morse['V'] = "...-"; char_to_morse['W'] = ".--"; char_to_morse['X'] = "-..-";
+    char_to_morse['Y'] = "-.--"; char_to_morse['Z'] = "--..";
     
     // DIGITS
-    char_to_morse['0'] = "-----";
-    char_to_morse['1'] = ".----";
-    char_to_morse['2'] = "..---";
-    char_to_morse['3'] = "...--";
-    char_to_morse['4'] = "....-";
-    char_to_morse['5'] = ".....";
-    char_to_morse['6'] = "-....";
-    char_to_morse['7'] = "--...";
-    char_to_morse['8'] = "---..";
-    char_to_morse['9'] = "----.";
+    char_to_morse['0'] = "-----"; char_to_morse['1'] = ".----"; char_to_morse['2'] = "..---"; char_to_morse['3'] = "...--";
+    char_to_morse['4'] = "....-"; char_to_morse['5'] = "....."; char_to_morse['6'] = "-...."; char_to_morse['7'] = "--...";
+    char_to_morse['8'] = "---.."; char_to_morse['9'] = "----.";
     
     // Special characters
     char_to_morse[' '] = "/";
 
-    // INIT
-    // Reverse mappings
     for (auto& pair : char_to_morse) {
         morse_to_char[pair.second] = pair.first;
     }
